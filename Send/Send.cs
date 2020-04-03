8a3b76c87d051c1afacefa41b5a85c61f98b3bf1
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2020-04-02 21:09:06 -0600

    Added the new messages

---
+++ Send/Send.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Send
+{
+    class Send
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "Hello", durable: false,
+                        exclusive: false, autoDelete: false,
+                        arguments: null);
+            var msg = "Hello world!!";
+            var body = Encoding.UTF8.GetBytes(msg);
+
+            channel.BasicPublish(exchange: "", routingKey: "Hello",
+                        basicProperties: null, body: body);
+
+            Console.WriteLine($"[x] Sent {msg}");
+
+            Console.WriteLine("Pres [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
